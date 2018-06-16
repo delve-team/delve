b02b7fce493d86e39009e4c56c0cdc9602e6e14d
Justin Shenk <shenkjustin@gmail.com>
2018-06-16 17:20:49 +0200

    Save data locally

--- example_deep.py
+++ example_deep.py
@@ -18,12 +18,12 @@ transform = transforms.Compose([
 batch_size = 128
 
 train_set = torchvision.datasets.CIFAR10(
-    root='/home/share/data', train=True, download=True, transform=transform)
+    root='./data', train=True, download=True, transform=transform)
 train_loader = torch.utils.data.DataLoader(
     train_set, batch_size=batch_size, shuffle=True, num_workers=2)
 
 test_set = torchvision.datasets.CIFAR10(
-    root='/home/share/data', train=False, download=True, transform=transform)
+    root='./data', train=False, download=True, transform=transform)
 test_loader = torch.utils.data.DataLoader(
     test_set, batch_size=batch_size, shuffle=False, num_workers=2)
 
