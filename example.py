14ec73caf98dc4e5d4d262e0d4234cb7d8d87047
Justin Shenk <shenkjustin@gmail.com>
2018-11-03 17:30:02 +0100

    Add shebang

--- example.py
+++ example.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import delve
 import logging
 import torch
@@ -22,7 +24,7 @@ class TwoLayerNet(torch.nn.Module):
 cuda = torch.cuda.is_available()
 torch.manual_seed(1)
 
-for h in [10, 100, 300]:
+for h in [3, 1000, 10000]:
     # N is batch size; D_in is input dimension;
     # H is hidden dimension; D_out is output dimension.
     N, D_in, H, D_out = 64, 1000, h, 10
