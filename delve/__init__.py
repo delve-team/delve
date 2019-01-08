ae49130452c5ba30c63c0c05ddfefd37f020d7d4
Justin Shenk <shenkjustin@gmail.com>
2019-01-08 18:39:21 +0100

    Move keras class to separate module

--- delve/__init__.py
+++ delve/__init__.py
@@ -5,14 +5,12 @@ try:
     from .main import CheckLayerSat
 except ImportError:
     print("Delve: Skipping PyTorch class import")
-    pass
 
 try:
     import keras
-    from .main import LayerSaturation
+    from .keras_classes import LayerSaturation
 except ImportError:
     print("Delve: Skipping Keras class import")
-    pass
 
 from .version import __version__
 
