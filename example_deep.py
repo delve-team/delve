3a96edc624d33d2fb28e30d9cd564a89bb698355
Cypher42 <matrichter@uos.de>
2019-03-13 18:27:49 +0100

    added jacobian

--- example_deep.py
+++ example_deep.py
@@ -5,9 +5,10 @@ import torch.nn.functional as F
 import torch.optim as optim
 import torchvision
 import torchvision.transforms as transforms
-
+from hessian import hessian
 from delve import CheckLayerSat
 from tqdm import tqdm, trange
+import time
 
 transform = transforms.Compose(
     [transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
@@ -19,16 +20,32 @@ train_set = torchvision.datasets.CIFAR10(
     root='./data', train=True, download=True, transform=transform
 )
 train_loader = torch.utils.data.DataLoader(
-    train_set, batch_size=batch_size, shuffle=True, num_workers=2
+    train_set, batch_size=batch_size, shuffle=True,
 )
 
 test_set = torchvision.datasets.CIFAR10(
     root='./data', train=False, download=True, transform=transform
 )
 test_loader = torch.utils.data.DataLoader(
-    test_set, batch_size=batch_size, shuffle=False, num_workers=2
+    test_set, batch_size=batch_size, shuffle=False,
 )
 
+
+def get_second_order_grad(grads, xs):
+    start = time.time()
+    grads2 = []
+    for j, (grad, x) in enumerate(zip(grads, xs)):
+        print('2nd order on ', j, 'th layer')
+        print(x.size())
+        grad = torch.reshape(grad, [-1])
+        grads2_tmp = []
+        for count, g in enumerate(grad):
+            g2 = torch.autograd.grad(g, x, retain_graph=True)[0]
+            g2 = torch.reshape(g2, [-1])
+            grads2_tmp.append(g2[count].data.cpu().numpy())
+        grads2.append(torch.from_numpy(np.reshape(grads2_tmp, x.size())))#.to(DEVICE_IDS[0]))
+        print('Time used is ', time.time() - start)
+    return grads2
 
 class Net(nn.Module):
     def __init__(self, h2):
@@ -59,7 +76,7 @@ epochs = 5
 
 for h2 in [8, 32, 128]:  # compare various hidden layer sizes
     net = Net(h2=h2)  # instantiate network with hidden layer size `h2`
-
+    #print(list(net.parameters()))
     net.to(device)
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
@@ -67,7 +84,7 @@ for h2 in [8, 32, 128]:  # compare various hidden layer sizes
     logging_dir = 'convNet/h2-{}'.format(h2)
     stats = CheckLayerSat(logging_dir, net)
     stats.write("CIFAR10 ConvNet - Changing fc2 - size {}".format(h2))  # optional
-
+    ys = torch.zeros(1, device=torch.device('cuda'))
     for epoch in range(epochs):
         running_loss = 0.0
         step = 0
@@ -82,14 +99,15 @@ for h2 in [8, 32, 128]:  # compare various hidden layer sizes
 
             outputs = net(inputs)
             loss = criterion(outputs, labels)
-            loss.backward()
+            ys = loss
+            loss.backward(retain_graph=True)
             optimizer.step()
 
             running_loss += loss.data
-            if i % 2000 == 1999:  # print every 2000 mini-batches
-                print('[%d, %5d] loss: %.3f' % (epoch + 1, i + 1, running_loss / 2000))
-                running_loss = 0.0
-                stats.add_scalar('batch_loss', running_loss, step)  # optional
+            #if i % 2000 == 1999:  # print every 2000 mini-batches
+            #    print('[%d, %5d] loss: %.3f' % (epoch + 1, i + 1, running_loss / 2000))
+            #    running_loss = 0.0
+            #    stats.add_scalar('batch_loss', running_loss, step)  # optional
 
             # update the training progress display
             loader.set_description(
@@ -97,6 +115,15 @@ for h2 in [8, 32, 128]:  # compare various hidden layer sizes
             )
             # display layer saturation levels
             stats.saturation()
+            optimizer.zero_grad()
+            xs = optimizer.param_groups[0]['params']
+           # jacobian = torch.autograd.grad(ys, xs, create_graph=True)  # first order gradient
+           # hessian = get_second_order_grad(jacobian, xs) # second order gradient
+            hess = hessian(loss, net.parameters())
+            print(hess)
+
+
+
 
     loader.write('\n')
     loader.close()
---
+++ hessian.py
@@ -0,0 +1,89 @@
+# pylint: disable=E1101, C0103, C0111
+'''
+Computes the Hessian
+'''
+import torch
+
+
+def gradient(outputs, inputs, grad_outputs=None, retain_graph=None, create_graph=False):
+    '''
+    Compute the gradient of `outputs` with respect to `inputs`
+    gradient(x.sum(), x)
+    gradient((x * y).sum(), [x, y])
+    '''
+    if torch.is_tensor(inputs):
+        inputs = [inputs]
+    else:
+        inputs = list(inputs)
+    grads = torch.autograd.grad(outputs, inputs, grad_outputs,
+                                allow_unused=True,
+                                retain_graph=retain_graph,
+                                create_graph=create_graph)
+    grads = [x if x is not None else torch.zeros_like(y) for x, y in zip(grads, inputs)]
+    return torch.cat([x.contiguous().view(-1) for x in grads])
+
+
+def jacobian(outputs, inputs, create_graph=False):
+    '''
+    Compute the Jacobian of `outputs` with respect to `inputs`
+    jacobian(x, x)
+    jacobian(x * y, [x, y])
+    jacobian([x * y, x.sqrt()], [x, y])
+    '''
+    if torch.is_tensor(outputs):
+        outputs = [outputs]
+    else:
+        outputs = list(outputs)
+
+    if torch.is_tensor(inputs):
+        inputs = [inputs]
+    else:
+        inputs = list(inputs)
+
+    jac = []
+    for output in outputs:
+        output_flat = output.view(-1)
+        output_grad = torch.zeros_like(output_flat)
+        for i in range(len(output_flat)):
+            output_grad[i] = 1
+            jac += [gradient(output_flat, inputs, output_grad, True, create_graph)]
+            output_grad[i] = 0
+    return torch.stack(jac)
+
+
+def hessian(output, inputs, out=None, allow_unused=False, create_graph=False):
+    '''
+    Compute the Hessian of `output` with respect to `inputs`
+    hessian((x * y).sum(), [x, y])
+    '''
+    assert output.ndimension() == 0
+
+    if torch.is_tensor(inputs):
+        inputs = [inputs]
+    else:
+        inputs = list(inputs)
+
+    n = sum(p.numel() for p in inputs)
+    if out is None:
+        out = output.new_zeros(n, n)
+
+    ai = 0
+    for i, inp in enumerate(inputs):
+        [grad] = torch.autograd.grad(output, inp, create_graph=True, allow_unused=allow_unused)
+        grad = torch.zeros_like(inp) if grad is None else grad
+        grad = grad.contiguous().view(-1)
+
+        for j in range(inp.numel()):
+            if grad[j].requires_grad:
+                row = gradient(grad[j], inputs[i:], retain_graph=True, create_graph=create_graph)[j:]
+            else:
+                row = grad[j].new_zeros(sum(x.numel() for x in inputs[i:]) - j)
+
+            out[ai, ai:].add_(row.type_as(out))  # ai's row
+            if ai + 1 < n:
+                out[ai + 1:, ai].add_(row[1:].type_as(out))  # ai's column
+            del row
+            ai += 1
+        del grad
+
+    return out
