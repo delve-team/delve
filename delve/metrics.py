Bugfix on Covariance Added PCA Layers

delve/metrics.py
@@ -1,72 +1,36 @@
 from typing import Dict, Union
 
 import numpy as np
+import torch
 
-COVARIANCE_MATRICES = dict()
 
-
-def get_eigenval_diversity_index(eig_vals: np.ndarray):
-    """Return Simpson diversity index of eigvalue explained variance ratios.
-    Args:
-        eig_vals   : eigenvalues in descending order
-
+def compute_intrinsic_dimensionality(cov: torch.Tensor, thresh: float = .99) -> int:
     """
-    tot = sum(eig_vals)
-    var_exp = [(i / tot) for i in eig_vals]
-    simpson_di = sum(x**2 for x in var_exp) * 100
-    return simpson_di
-
-
-def get_explained_variance(eig_vals: np.ndarray,
-                           threshold: float = 0.99,
-                           return_cum: bool = False):
-    """Return number of `eig_vals` needed to explain `threshold`*100 percent of variance
-    Args:
-        eig_vals   : numpy.ndarray of eigenvalues in descending order
-        threshold  : percent of variance to explain
-        return_cum : return the cumulative array of eigenvalues
-    Return:
-        nr_eig_vals : int
-        cum_var_exp : numpy.ndarray
-
-            or
-
-        nr_eig_vals : int
+    Compute the intrinsic dimensionality based on the covariance matrix
+    :param cov: the covariance matrix as a torch tensor
+    :param thresh: the delta value for the explained variance of the covariance matrix
+    :return: The intrinsic dimensionality, which is a single integer value greater than 0
     """
-    tot = sum(eig_vals)
-    var_exp = [(i / tot) for i in eig_vals]
-    cum_var_exp = np.cumsum(var_exp)
-    nr_eig_vals = np.argmax(cum_var_exp > threshold)
-    if return_cum:
-        return nr_eig_vals, cum_var_exp
-    return nr_eig_vals
+    eig_vals, eigen_space = cov.symeig(True)
+    eig_vals, idx = eig_vals.sort(descending=True)
+    eig_vals[eig_vals < 0] = 0
+    percentages = eig_vals.cumsum(0) / eig_vals.sum()
+    eigen_space = eigen_space[:, percentages < thresh]
+    if eigen_space.shape[1] == 0:
+        eigen_space = eigen_space[:, :1]
+    elif thresh - (percentages[percentages < thresh][-1]) > 0.02:
+        eigen_space = eigen_space[:, :eigen_space.shape[1] + 1]
+    return eigen_space.shape[1]
 
 
-def get_layer_saturation(nr_eig_vals: np.ndarray, layer_width: int) -> int:
-    saturation = round(100 * nr_eig_vals / layer_width, 2)
-    return saturation
-
-def batch_mean(batch: np.ndarray):
-    """Get mean of first vector in `batch`."""  # TODO: Add support for non-dense layers.
-    return np.mean(batch[0])
-
-def batch_cov(batch: np.ndarray):
-    """Get covariance of first instance in `batch`."""  # TODO: Add support for non-dense layers.
-    return np.cov(batch[0])
-
-def compute_saturation(cov: np.ndarray, thresh: float = .99) -> float:
+def compute_saturation(cov: torch.Tensor, thresh: float = .99) -> float:
     """
-    Provides layer saturation
-    :param covariance_matrix:
-    :param thresh:
-    :param conv_method:
-    :return:
+    Computes the saturation
+    :param cov: the covariance matrix as a torch tensor
+    :param thresh: the delta value for the explained variance of the covariance matrix
+    :return: a value between 0 and 1
     """
-    
-    eig_vals = np.linalg.eigvalsh(cov)
+    intrinsic_dimensionality = compute_intrinsic_dimensionality(cov, thresh)
+    feature_space_dimensionality = cov.shape[0]
 
-    # Sort the eigenvalues from high to low
-    eig_vals = sorted(eig_vals, reverse=True)
-    total_dim = len(cov)
-    nr_eigs = get_explained_variance(eig_vals=eig_vals, threshold=thresh, return_cum=False)
-    return get_layer_saturation(nr_eig_vals=nr_eigs, layer_width=total_dim)
+    return intrinsic_dimensionality / feature_space_dimensionality

delve/torch_utils.py
@@ -37,10 +37,7 @@ class TorchCovarianceMatrix(object):
         self._tlen = 0
 
     def update(self, x, vae):
-        """Update internal structures.
-
-        Note that no consistency checks are performed on the data (this is
-        typically done in the enclosing node).
+        """Update internal structures given a batch of data
         """
         x = x.type(torch.float64).to(device=self.device)
         if self._cov_mtx is None:
@@ -60,12 +57,7 @@ class TorchCovarianceMatrix(object):
         self._tlen += x.shape[0]
 
     def fix(self, center=True):
-        """Returns a triple containing the covariance matrix, the average and
-        the number of observations. The covariance matrix is then reset to
-        a zero-state.
-
-        If center is false, the returned matrix is the matrix of the second moments,
-        i.e. the covariance matrix of the data without subtracting the mean."""
+        """Returns the Covariance matrix"""
         # local variables
         tlen = self._tlen
         cov_mtx = self._cov_mtx

delve/torchcallback.py
@@ -15,7 +15,6 @@ import delve
 logging.basicConfig(format='%(levelname)s:delve:%(message)s',
                     level=logging.INFO)
 
-
 class CheckLayerSat(object):
     """Takes PyTorch layers, layer or model as `modules` and writes tensorboardX
     summaries to `logging_dir`. Outputs layer saturation with call to self.saturation().
@@ -126,6 +125,8 @@ class CheckLayerSat(object):
             stats = list(stats)
         supported_stats = [
             'lsat',
+            'idim',
+            'cov',
         ]
         compatible = [stat in supported_stats for stat in stats]
         incompatible = [i for i, x in enumerate(compatible) if not x]
@@ -197,6 +198,33 @@ class CheckLayerSat(object):
         self.register_forward_hooks(layer, self.stats)
         return self
 
+    def _record_stat(self, activations_batch: torch.Tensor, lstm_ae: bool, layer: torch.nn.Module, training_state: str, stat: str):
+        if activations_batch.dim() == 4:  # conv layer (B x C x H x W)
+            if self.conv_method == 'median':
+                shape = activations_batch.shape
+                reshaped_batch = activations_batch.reshape(shape[0], shape[1], shape[2] * shape[3])
+                activations_batch, _ = torch.median(reshaped_batch, dim=2)  # channel median
+            elif self.conv_method == 'max':
+                shape = activations_batch.shape
+                reshaped_batch = activations_batch.reshape(shape[0], shape[1], shape[2] * shape[3])
+                activations_batch, _ = torch.max(reshaped_batch, dim=2)  # channel median
+            elif self.conv_method == 'mean':
+                activations_batch = torch.mean(activations_batch, dim=(2, 3))
+            elif self.conv_method == 'flatten':
+                activations_batch = activations_batch.view(activations_batch.size(0), -1)
+            elif self.conv_method == 'channelwise':
+                reshaped_batch: torch.Tensor = activations_batch.permute([1, 0, 2, 3])
+                shape = reshaped_batch.shape
+                reshaped_batch: torch.Tensor = reshaped_batch.flatten(1)
+                reshaped_batch: torch.Tensor = reshaped_batch.permute([1, 0])
+                activations_batch = reshaped_batch
+
+        if layer.name in self.logs[f'{training_state}-saturation']:
+            self.logs[f'{training_state}-{stat}'][layer.name].update(activations_batch, lstm_ae)
+        else:
+            self.logs[f'{training_state}-{stat}'][layer.name] = TorchCovarianceMatrix(device=self.device)
+            self.logs[f'{training_state}-{stat}'][layer.name].update(activations_batch, lstm_ae)
+
     def register_forward_hooks(self, layer: torch.nn.Module, stats: list):
         """Register hook to show `stats` in `layer`."""
 
@@ -225,36 +253,13 @@ class CheckLayerSat(object):
                 eig_vals = None
 
                 if 'lsat' in stats:
-                    
-                    if activations_batch.dim() == 4:  # conv layer (B x C x H x W)
-                        if self.conv_method == 'median':
-                            shape = activations_batch.shape
-                            reshaped_batch = activations_batch.reshape(shape[0], shape[1], shape[2]*shape[3])
-                            activations_batch, _ = torch.median(reshaped_batch, dim=2)  # channel median
-                        elif self.conv_method == 'max':
-                            shape = activations_batch.shape
-                            reshaped_batch = activations_batch.reshape(shape[0], shape[1], shape[2]*shape[3])
-                            activations_batch, _ = torch.max(reshaped_batch, dim=2)  # channel median
-                        elif self.conv_method == 'mean':
-                            activations_batch = torch.mean(activations_batch, dim=(2, 3))
-                        elif self.conv_method == 'flatten':
-                            activations_batch = activations_batch.view(activations_batch.size(0), -1)
-                        elif self.conv_method == 'channelwise':
-                            reshaped_batch: torch.Tensor = activations_batch.permute([1, 0, 2, 3])
-                            shape = reshaped_batch.shape
-                            reshaped_batch: torch.Tensor = reshaped_batch.flatten(1)
-                            reshaped_batch: torch.Tensor = reshaped_batch.permute([1, 0])
-                            activations_batch = reshaped_batch
-
-                    if layer.name in self.logs[f'{training_state}-saturation']:
-                        self.logs[f'{training_state}-saturation'][layer.name].update(activations_batch, lstm_ae)
-                    else:
-                        self.logs[f'{training_state}-saturation'][layer.name] = TorchCovarianceMatrix(device=self.device)
-                        self.logs[f'{training_state}-saturation'][layer.name].update(activations_batch, lstm_ae)
-
+                    self._record_stat(activations_batch, lstm_ae, layer, training_state, 'saturation')
+                if 'idim' in stats:
+                    self._record_stat(activations_batch, lstm_ae, layer, training_state, 'intrinsic_dimensionality')
+                if 'cov' in stats:
+                    self._record_stat(activations_batch, lstm_ae, layer, training_state, 'covariance_matrix')
 
         layer.register_forward_hook(record_layer_saturation)
-
 
     def add_saturations(self, save=True):
         """
@@ -270,7 +275,7 @@ class CheckLayerSat(object):
                         continue
                     if self.logs[key][layer_name]._cov_mtx is None:
                         raise ValueError('Attempting to compute saturation when covariance is not initialized')
-                    sat = compute_saturation(self.logs[key][layer_name].fix().cpu().numpy(), thresh=self.threshold)
+                    sat = compute_saturation(self.logs[key][layer_name].fix(), thresh=self.threshold)
                     self.seen_samples[key.split('-')[0]][layer_name] = 0
                     if self.reset_covariance:
                         self.logs[key][layer_name]._cov_mtx = None
@@ -281,6 +286,32 @@ class CheckLayerSat(object):
                         val_sats.append(sat)
                     elif 'train' in key:
                         train_sats.append(sat)
+            elif '-intrinsic' in key:
+                for i, layer_name in enumerate(self.logs[key]):
+                    if layer_name in self.ignore_layer_names:
+                        continue
+                    if self.logs[key][layer_name]._cov_mtx is None:
+                        raise ValueError('Attempting to compute intrinsic dimensionality when covariance is not initialized')
+                    intrinsic = compute_intrinsic_dimensionality(self.logs[key][layer_name].fix(), thresh=self.threshold)
+                    self.seen_samples[key.split('-')[0]][layer_name] = 0
+                    if self.reset_covariance:
+                        self.logs[key][layer_name]._cov_mtx = None
+                    if self.layerwise_sat:
+                        name = key+'_'+layer_name
+                        self.writer.add_scalar(name, intrinsic)
+            elif '-covariance' in key:
+                for i, layer_name in enumerate(self.logs[key]):
+                    if layer_name in self.ignore_layer_names:
+                        continue
+                    if self.logs[key][layer_name]._cov_mtx is None:
+                        raise ValueError('Attempting to compute intrinsic dimensionality when covariance is not initialized')
+                    cov_mat = self.logs[key][layer_name].fix()
+                    self.seen_samples[key.split('-')[0]][layer_name] = 0
+                    if self.reset_covariance:
+                        self.logs[key][layer_name]._cov_mtx = None
+                    if self.layerwise_sat:
+                        name = key+'_'+layer_name
+                        self.writer.add_scalar(name, cov_mat)
 
         if self.average_sat:
             self.writer.add_scalar('average_train_sat', np.mean(train_sats))
@@ -288,7 +319,6 @@ class CheckLayerSat(object):
 
         if save:
             self.save()
-
 
     def save(self):
         self.writer.save()