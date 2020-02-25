Bugfix on Covariance Added PCA Layers

delve/torchcallback.py
@@ -2,6 +2,7 @@ import logging
 from typing import List, Dict, Any, Optional, Union
 import torch
 from collections import OrderedDict
+from itertools import product
 from torch.nn.modules.activation import ReLU
 from torch.nn.modules.conv import Conv2d
 from torch.nn.modules.linear import Linear
@@ -77,14 +78,11 @@ class CheckLayerSat(object):
 
         self.writer = self._get_writer(save_to, writer_args)
         self.interval = log_interval
-        self.stats = self._check_stats(stats)
+
+        self.logs, self.stats = self._check_stats(stats)
         self.layerwise_sat = layerwise_sat
         self.average_sat = average_sat
         self.ignore_layer_names = ignore_layer_names
-        self.logs = {
-            'eval-saturation': OrderedDict(),
-            'train-saturation': OrderedDict()
-        }
         self.seen_samples = {
             'train': {},
             'eval': {}
@@ -132,7 +130,19 @@ class CheckLayerSat(object):
         incompatible = [i for i, x in enumerate(compatible) if not x]
         assert all(compatible), "Stat {} is not supported".format(
             stats[incompatible[0]])
-        return stats
+
+        name_mapper = {
+            'lsat': 'saturation',
+            'idim': 'intrinsic-dimensionality',
+            'cov': 'covariance-matrix'
+        }
+
+        logs = {
+            f'{mode}-{name_mapper[stat]}': OrderedDict()
+            for mode, stat in product(['train', 'eval'], stats)
+        }
+
+        return logs, stats
 
     def _add_conv_layer(self, layer: torch.nn.Module):
         layer.out_features = layer.out_channels
@@ -219,7 +229,7 @@ class CheckLayerSat(object):
                 reshaped_batch: torch.Tensor = reshaped_batch.permute([1, 0])
                 activations_batch = reshaped_batch
 
-        if layer.name in self.logs[f'{training_state}-saturation']:
+        if layer.name in self.logs[f'{training_state}-{stat}']:
             self.logs[f'{training_state}-{stat}'][layer.name].update(activations_batch, lstm_ae)
         else:
             self.logs[f'{training_state}-{stat}'][layer.name] = TorchCovarianceMatrix(device=self.device)
@@ -255,9 +265,9 @@ class CheckLayerSat(object):
                 if 'lsat' in stats:
                     self._record_stat(activations_batch, lstm_ae, layer, training_state, 'saturation')
                 if 'idim' in stats:
-                    self._record_stat(activations_batch, lstm_ae, layer, training_state, 'intrinsic_dimensionality')
+                    self._record_stat(activations_batch, lstm_ae, layer, training_state, 'intrinsic-dimensionality')
                 if 'cov' in stats:
-                    self._record_stat(activations_batch, lstm_ae, layer, training_state, 'covariance_matrix')
+                    self._record_stat(activations_batch, lstm_ae, layer, training_state, 'covariance-matrix')
 
         layer.register_forward_hook(record_layer_saturation)
 
@@ -309,13 +319,13 @@ class CheckLayerSat(object):
                     self.seen_samples[key.split('-')[0]][layer_name] = 0
                     if self.reset_covariance:
                         self.logs[key][layer_name]._cov_mtx = None
-                    if self.layerwise_sat:
-                        name = key+'_'+layer_name
-                        self.writer.add_scalar(name, cov_mat)
+                    #if self.layerwise_sat:
+                    #    name = key+'_'+layer_name
+                    #    self.writer.add_scalar(name, cov_mat)
 
         if self.average_sat:
-            self.writer.add_scalar('average_train_sat', np.mean(train_sats))
-            self.writer.add_scalar('average_eval_sat', np.mean(val_sats))
+            self.writer.add_scalar('average-train-sat', np.mean(train_sats))
+            self.writer.add_scalar('average-eval-sat', np.mean(val_sats))
 
         if save:
             self.save()

delve/writers.py
@@ -3,12 +3,23 @@ This file contains alternative file writers
 """
 from abc import ABC, abstractmethod
 from matplotlib import pyplot as plt
+from shutil import make_archive
+from typing import Callable
 import pathlib
 import pandas as pd
+import numpy as np
+import os
+
 try:
     from tensorboardX import SummaryWriter
 except ModuleNotFoundError:
     pass
+
+STATMAP = {
+    'idim': 'intrinsic-dimensionality',
+    'sat': 'saturation',
+    'cov': 'covariance_matrix'
+}
 
 
 class AbstractWriter(ABC):
@@ -49,30 +60,50 @@ class CSVWriter(AbstractWriter):
             self.add_scalar(name, value_dict[name])
 
     def save(self):
-        pd.DataFrame.from_dict(self.value_dict).to_csv(self.savepath+'.csv', sep=';')
+        pd.DataFrame.from_dict(self.value_dict).to_csv(self.savepath + '.csv', sep=';')
 
     def close(self):
         pass
 
 
-class NPZWriter(AbstractWriter):
+class NPYWriter(AbstractWriter):
 
-    def __init__(self, savepath: str, **kwargs):
-        super(NPZWriter, self).__init__()
+    def __init__(self, savepath: str, zip: bool = False, **kwargs):
+        super(NPYWriter, self).__init__()
         self.savepath = savepath
-        self.epoch_counter = 0
+        self.epoch_counter = {}
+        self.zip = zip
+
+    def _update_epoch_counter(self, name: str) -> int:
+        if name not in self.epoch_counter:
+            self.epoch_counter[name] = 0
+        else:
+            self.epoch_counter[name] += 1
+        return self.epoch_counter[name]
+
+    def _get_and_create_savepath(self, name: str, epoch) -> str:
+        savepath = os.path.join(self.savepath, name)
+        if not os.path.exists(savepath):
+            os.makedirs(savepath)
+        return os.path.join(savepath, name + f'_epoch{epoch}')
 
     def add_scalar(self, name, value, **kwargs):
-        #TODO: save as npz-file
-        pass
+        epoch = self._update_epoch_counter(name)
+        svpth = self._get_and_create_savepath(name, epoch)
+        np.save(svpth, value, **kwargs)
 
     def add_scalars(self, prefix, value_dict, **kwargs):
-        #TODO: save as npz
         for key in value_dict.keys():
-            self.add_scalar(prefix+'_'+key, value_dict[key])
+            self.add_scalar(prefix + '_' + key, value_dict[key])
 
     def save(self):
-        pass
+        if self.zip:
+            make_archive(
+                base_name=os.path.basename(self.savepath),
+                format='zip',
+                root_dir=os.path.dirname(self.savepath),
+                verbose=True
+            )
 
     def close(self):
         pass
@@ -88,7 +119,7 @@ class PrintWriter(AbstractWriter):
 
     def add_scalars(self, prefix, value_dict, **kwargs):
         for key in value_dict.keys():
-            self.add_scalar(prefix+'_'+key, value_dict[key])
+            self.add_scalar(prefix + '_' + key, value_dict[key])
 
     def save(self):
         pass
@@ -119,12 +150,37 @@ class TensorBoardWriter(AbstractWriter):
 
 class CSVandPlottingWriter(CSVWriter):
 
-    def __init__(self, savepath: str, **kwargs):
+    def __init__(self, savepath: str, plot_manipulation_func: Callable[[plt.Axes], plt.Axes] = None, **kwargs):
+        """
+        This writer produces the CSV similar to the CSV writer, but also provides plots, which are per default stored in
+        the subfolder.
+        :param savepath: path to store images and csvs
+        :param plot_manipulation_func: this is a function mapping an axis object to an axis object, you can use this to
+                                       to manipulate the plot even further, by writing arbitrary pyplot code in this
+                                       function
+        :param kwargs:
+        """
         super(CSVandPlottingWriter, self).__init__(savepath)
+        self.plot_man_func = plot_manipulation_func if plot_manipulation_func is not None else lambda x: x
         self.primary_metric = None if not 'primary_metric' in kwargs else kwargs['primary_metric']
         self.fontsize = 16 if not 'fontsize' in kwargs else kwargs['fontsize']
         self.figsize = None if not 'figsize' in kwargs else kwargs['figsize']
         self.epoch_counter: int = 0
+        self.stats = []
+
+    def _look_for_stats(self):
+        if len(self.stats) == 0:
+            sat = False
+            idim = False
+            for key in self.value_dict.keys():
+                if 'saturation' in key:
+                    sat = True
+                if 'intrinsic-dimensionality' in key:
+                    idim = True
+            if sat:
+                self.stats.append('sat')
+            if idim:
+                self.stats.append('idim')
 
     def add_scalar(self, name, value, **kwargs):
         if name in self.value_dict:
@@ -138,46 +194,76 @@ class CSVandPlottingWriter(CSVWriter):
             self.add_scalar(name, value_dict[name])
 
     def save(self):
-        pd.DataFrame.from_dict(self.value_dict).to_csv(self.savepath+'.csv', sep=';')
-        plot_saturation_level_from_results(self.savepath+'.csv', epoch=self.epoch_counter, primary_metric=self.primary_metric, fontsize=self.fontsize, figsize=self.figsize)
+        self._look_for_stats()
+        pd.DataFrame.from_dict(self.value_dict).to_csv(self.savepath + '.csv', sep=';')
+        for stat in self.stats:
+            plot_stat_level_from_results(self.savepath + '.csv', stat=stat, epoch=self.epoch_counter,
+                                         primary_metric=self.primary_metric, fontsize=self.fontsize,
+                                         figsize=self.figsize)
         self.epoch_counter += 1
 
     def close(self):
         pass
 
 
-def extract_layer_saturation(df, epoch=19, primary_metric = None):
+def extract_layer_stat(df, epoch=19, primary_metric=None, stat='saturation'):
     cols = list(df.columns)
     train_cols = [col for col in cols if
-                  'train' in col and not 'accuracy' in col and not 'loss' in col]
+                  'train' in col and not 'accuracy' in col and stat in col]
     epoch_df = df[df.index.values == epoch]
     pm = None if primary_metric is None else epoch_df[primary_metric].values[0]
     epoch_df = epoch_df[train_cols]
     return epoch_df, pm
 
 
-def plot_saturation_level(df, pm=-1, savepath='run.png', epoch=0, primary_metric=None, fontsize=16, figsize=None):
+def plot_stat(df, stat, pm=-1, savepath='run.png', epoch=0, primary_metric=None, fontsize=16, figsize=None,
+              line=True, scatter=True, ylim=(0, 1.0), alpha_line=.6, alpha_scatter=1.0, color_line=None,
+              color_scatter=None,
+              primary_metric_loc=(0.7, 0.8), show_col_label_x=True, show_col_label_y=True, show_grid=True, save=True):
     plt.clf()
     if figsize is not None:
         print(figsize)
         plt.figure(figsize=figsize)
     ax = plt.gca()
     col_names = [i for i in df.columns]
-    ax.bar(list(range(len(col_names))), df.values[0])
-    plt.xticks(list(range(len(col_names))), [col_name.replace('train-saturation_', '') for col_name in col_names], rotation=90)
-    ax.set_ylim((0, 100))
+    if line:
+        ax.plot(list(range(len(col_names))), df.values[0], alpha=alpha_line, color=color_line)
+    if scatter:
+        ax.scatter(list(range(len(col_names))), df.values[0], alpha=alpha_scatter, color=color_scatter)
+    plt.xticks(list(range(len(col_names))), [col_name.split('_', )[1] for col_name in col_names],
+               rotation=90)
+    if not ylim is None:
+        ax.set_ylim(ylim)
     if primary_metric is not None:
-        ax.text(1, 80, f'{primary_metric}: {pm}')
+        ax.text(primary_metric_loc[0], primary_metric_loc[1], f'{primary_metric}: {pm}')
     plt.yticks(fontsize=fontsize)
-    plt.xlabel('Layers', fontsize=fontsize)
+    if show_col_label_x:
+        plt.xlabel('layers', fontsize=fontsize)
     plt.title(pathlib.Path(savepath).name.replace('_', ' ').replace('.csv', f' epoch: {epoch}'), fontsize=fontsize)
-    plt.ylabel('Saturation in %', rotation='vertical', fontsize=fontsize)
+    if show_col_label_y:
+        plt.ylabel(stat if not stat in STATMAP else STATMAP[stat], rotation='vertical', fontsize=fontsize)
+    if show_grid:
+        plt.grid()
     plt.tight_layout()
-    plt.savefig(savepath.replace('.csv', f'_epoch_{epoch}.png'))
-    return
+    if save:
+        final_savepath = savepath.replace('.csv', f'{stat}_epoch_{epoch}.png')
+        print(final_savepath)
+        plt.savefig(final_savepath)
+    return ax
 
 
-def plot_saturation_level_from_results(savepath, epoch, primary_metric=None, fontsize=16, figsize=None):
+def plot_stat_level_from_results(savepath, epoch, stat, primary_metric=None, fontsize=16, figsize=None, line=True,
+                                 scatter=True, ylim=(0, 1.0), alpha_line=.6, alpha_scatter=1.0, color_line=None,
+                                 color_scatter=None,
+                                 primary_metric_loc=(0.7, 0.8), show_col_label_x=True, show_col_label_y=True,
+                                 show_grid=True, save=True):
     df = pd.read_csv(savepath, sep=';')
-    epoch_df, pm = extract_layer_saturation(df, epoch=epoch, primary_metric=primary_metric)
-    plot_saturation_level(epoch_df, pm, savepath, epoch, primary_metric=primary_metric, fontsize=fontsize, figsize=figsize)
+    epoch_df, pm = extract_layer_stat(df, stat=STATMAP[stat], epoch=epoch, primary_metric=primary_metric)
+    ax = plot_stat(df=epoch_df, pm=pm, savepath=savepath, epoch=epoch, primary_metric=primary_metric, fontsize=fontsize,
+                   figsize=figsize, stat=stat, ylim=None if stat is 'idim' else (0, 1.0), line=line, scatter=scatter,
+                   alpha_line=alpha_line, alpha_scatter=alpha_scatter, color_line=color_line,
+                   color_scatter=color_scatter,
+                   primary_metric_loc=primary_metric_loc, show_col_label_x=show_col_label_x,
+                   show_col_label_y=show_col_label_y,
+                   show_grid=show_grid, save=save)
+    return ax